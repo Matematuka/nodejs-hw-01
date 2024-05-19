6701d42338b07c91ebd46220d5591b111cc21501
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-05-19 15:06:51 +0300

    removeAll

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -3,9 +3,7 @@ import fs from 'fs/promises';
 
 export const removeAllContacts = async () => {
   try {
-    const allContacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
-    const deleteContacts = allContacts.splice(0);
-    await fs.writeFile(PATH_DB, JSON.stringify(deleteContacts));
+    await fs.writeFile(PATH_DB, JSON.stringify([], null));
     console.log('Successfully removed all contacts');
   } catch (error) {
     console.log(error, 'Failed to remove contacts');
